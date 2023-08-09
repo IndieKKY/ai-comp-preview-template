e536ad8c57e9f3b595da86b7d85a625482fed4c0
IndieKKY <121992849+IndieKKY@users.noreply.github.com>
2023-08-09 09:39:14 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,24 @@ link.rel = "stylesheet";
 link.href = "https://unpkg.com/@tailwindcss/ui@0.7.2/dist/tailwind-ui.min.css";
 document.head.appendChild(link);
 
+const stylesheet = document.createElement("style");
+stylesheet.innerHTML = `
+  ::-webkit-scrollbar {
+    display: none;
+    width: 0 !important;
+    background: transparent;
+  }
+`;
+document.head.appendChild(stylesheet);
+
 export default function App() {
   return (
     <div className="w-full p-3">
-      <div id='CompWrapper'>
+      <div id='CompWrapper' style={{
+        maxHeight: '400px',
+        overflowX: 'hidden',
+        overflowY: 'auto',
+      }}>
         <Comp data={Example} />
       </div>
     </div>
